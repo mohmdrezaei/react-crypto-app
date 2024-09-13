7cc2723e05220ea3e74642cd90d436fb05f516c4
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-13 15:38:44 +0330

    Implementation of the pagination component

---
+++ src/components/modules/Pagination.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function Pagination() {
+  const [page, setPage] = useState(1);
+  const previosHandler = () => {
+    if(page <= 1) return;
+    setPage((page) => page - 1);
+  };
+  const nextHandler = () => {
+    if(page >= 10) return;
+    setPage((page) => page + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={previosHandler}>Previos</button>
+      <p style={{color : page === 1 ? "red" : "inherit"}}>1</p>
+      <p style={{color : page === 2 ? "red" : "inherit"}}>2</p>
+      {page > 2 && page < 9 && (
+        <>
+        <span>...</span>
+        <p>{page}</p>
+        </>
+      ) }
+      <p>...</p>
+      <p style={{color : page === 9 ? "red" : "inherit"}}>9</p>
+      <p style={{color : page === 10 ? "red" : "inherit"}}>10</p>
+      <button onClick={nextHandler}>Next</button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
-  const [isLoading , setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const getData = async () => {
       const res = await fetch(getCoinList());
       const json = await res.json();
       setCoins(json);
-      setIsLoading(false)
+      setIsLoading(false);
     };
     getData();
   }, []);
   return (
     <div>
-      <TableCoin coins={coins} isLoading={isLoading}/>
+      <Pagination />
+      <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
 }
