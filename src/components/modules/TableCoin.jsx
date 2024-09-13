798b4548d9c80518f9548e206dd0429f2e9b34c0
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-13 11:42:41 +0330

    Extract TableRow

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -2,7 +2,6 @@ import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 
 function TableCoin({ coins }) {
-  console.log(coins);
   return (
     <div>
       <table>
@@ -18,26 +17,7 @@ function TableCoin({ coins }) {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <div>
-                  <img src={coin.image} alt="" />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td>
-              <td>{coin.name}</td>
-              <td>${coin.current_price.toLocaleString()}</td>
-              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-              <td>{coin.total_volume.toLocaleString()}</td>
-              <td>
-                <img
-                  src={
-                    coin.price_change_percentage_24h > 0 ? chartUp : chartDown
-                  }
-                  alt=""
-                />
-              </td>
-            </tr>
+            <TableRow coin={coin} key={coin.id} />
           ))}
         </tbody>
       </table>
@@ -46,3 +26,35 @@ function TableCoin({ coins }) {
 }
 
 export default TableCoin;
+
+const TableRow = ({
+  coin: {
+    name,
+    image,
+    symbol,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: price_change
+  },
+}) => {
+  return (
+    <tr >
+      <td>
+        <div>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img
+          src={price_change > 0 ? chartUp : chartDown}
+          alt={name}
+        />
+      </td>
+    </tr>
+  );
+};
