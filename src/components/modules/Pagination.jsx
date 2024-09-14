692f007e16e89753f64c3a109f8f14dd2c6b29ce
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-15 01:29:38 +0330

    Styling the pagination component

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,31 +1,40 @@
+import styles from "./Pagination.module.css";
 
-
-function Pagination({page , setPage}) {
- 
+function Pagination({ page, setPage }) {
   const previosHandler = () => {
-    if(page <= 1) return;
+    if (page <= 1) return;
     setPage((page) => page - 1);
   };
   const nextHandler = () => {
-    if(page >= 10) return;
+    if (page >= 10) return;
     setPage((page) => page + 1);
   };
 
   return (
-    <div>
-      <button onClick={previosHandler}>Previos</button>
-      <p style={{color : page === 1 ? "red" : "inherit"}}>1</p>
-      <p style={{color : page === 2 ? "red" : "inherit"}}>2</p>
+    <div className={styles.pagination}>
+      <button
+        onClick={previosHandler}
+        className={page === 1 ? styles.disabled : null}
+      >
+        Previos
+      </button>
+      <p className={page === 1 ? styles.selected : null}>1</p>
+      <p className={page === 2? styles.selected : null}>2</p>
       {page > 2 && page < 9 && (
         <>
-        <span>...</span>
-        <p>{page}</p>
+          <span>...</span>
+          <p className={styles.selected}>{page}</p>
         </>
-      ) }
-      <p>...</p>
-      <p style={{color : page === 9 ? "red" : "inherit"}}>9</p>
-      <p style={{color : page === 10 ? "red" : "inherit"}}>10</p>
-      <button onClick={nextHandler}>Next</button>
+      )}
+      <span>...</span>
+      <p className={page === 9 ? styles.selected : null}>9</p>
+      <p className={page === 10 ? styles.selected : null}>10</p>
+      <button
+        onClick={nextHandler}
+        className={page === 10 ? styles.disabled : null}
+      >
+        Next
+      </button>
     </div>
   );
 }
